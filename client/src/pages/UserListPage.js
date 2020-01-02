41b35fc56428b3d3281952a62f0670c24c5ef5a1
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:48:32 +0900

    [ADD] UserLIstPage

---
+++ client/src/pages/UserListPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import UserList from '../containers/UserList/UserList';
+
+class UserList extends React.Component {
+    render() {
+        return (
+            <UserList/>
+        );
+    };
+};
+
+export default UserList;
