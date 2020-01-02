e794860c9aa72c3b0ca3cc01147b303fc2b50bcf
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:48:23 +0900

    [ADD] Login Store

--- client/src/stores/login.js
+++ client/src/stores/login.js
@@ -1,9 +1,18 @@
-import { observable } from 'mobx';
+import { observable, action } from 'mobx';
 
-class login {
+export default class login {
     @observable id = "";
     @observable pw = "";
 
     @action changeId = (txt) => this.id = txt;
     @action changePw = (txt) => this.pw = txt;
-}
+
+    @action login = () => {
+        if(this.id === "admin" && this.pw === "1234") {
+            console.log("Success");
+            return true;
+        }
+        console.log("Fail")
+        return false;
+    }
+};
