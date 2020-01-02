bedd0ddf2603253355e721c58e4c682dc6df7870
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:48:12 +0900

    [UPDATE] Route UserList

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Main from './pages/MainPage';
+import UserList from './containers/UserList/UserList';
 
 class App extends React.Component {
     render() {
         return (
             <Router>
-                <Route exact path="/" component={Main}/>
+                <Route exact path="/" component={Main} />
+                <Route path="/userList" component={UserList} />
             </Router>
         );
     };
