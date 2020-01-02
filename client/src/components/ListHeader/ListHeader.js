ec00f55e59d89a0f15175f2068752082a39f946b
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:47:05 +0900

    [ADD] ListHeader Component

---
+++ client/src/components/ListHeader/ListHeader.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './ListHeader.css';
+
+class ListHeader extends React.Component {
+    render() {
+        const { width, textAlign } = this.props;
+        console.log(this.props.children)
+        return (
+            <div className="listHeader" style={{width: width}}>
+                {this.props.children.map(item => {
+                    return (
+                        <div className="title" style={{width: item.props.width, textAlign: textAlign}}>
+                            {item.props.children}
+                        </div>
+                    )
+                })}
+            </div>
+        );
+    };
+};
+
+export default ListHeader;
