1093f43a12d6c172c96598814695fda1b7bd5b09
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:46:43 +0900

    [FIX] Add onKeyUp Event

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -3,9 +3,9 @@ import './Input.css';
 
 class Input extends React.Component {
     render() {
-        const { width, height, type, placeholder, onChange } = this.props;
+        const { width, height, type, placeholder, onChange, onKeyUp } = this.props;
         return (
-            <input className="input" style={{"width": width, "height": height}} type={type} placeholder={placeholder} onChange={onChange} />
+            <input className="input" style={{"width": width, "height": height}} type={type} placeholder={placeholder} onChange={onChange} onKeyUp={onKeyUp} />
         );
     };
 };
