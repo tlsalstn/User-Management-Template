61a47e2a032e1131e65ddb537a559db5fdc32b81
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-02 18:47:39 +0900

    [ADD] Login Store

--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'mobx-react';
+import login from './stores/login';
+
+const loginStore = new login();
 
 ReactDOM.render(
-    <Provider>
+    <Provider login={loginStore}>
         <App />
     </Provider>,
     document.getElementById('root')
