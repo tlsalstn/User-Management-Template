7ed6cc85556e37eb6e1a41a5e05318d6cfcfa655
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-05 15:27:35 +0900

    [UPDATE] UserList

--- client/src/containers/UserList/UserList.js
+++ client/src/containers/UserList/UserList.js
@@ -2,24 +2,38 @@ import React from 'react';
 import './UserList.css';
 import ListHeader from '../../components/ListHeader/ListHeader';
 import ListBody from '../../components/ListBody/ListBody';
+import { inject, observer } from 'mobx-react';
 
+@inject('user')
+@observer
 class UserList extends React.Component {
+    UNSAFE_componentWillMount() {
+        this.props.user.getUsers();
+    }
+
     render() {
+        const { users } = this.props.user;
+        console.log(users);
+
         return (
             <div className="userList">
                 <div className="list">
                     <ListHeader textAlign="center">
-                        <span>ID</span>
-                        <span>Password</span>
-                        <span>Name</span>
-                        <span>Email</span>
+                        <span width="10%">ID</span>
+                        <span width="50%">Password</span>
+                        <span width="10%">Name</span>
+                        <span width="30%">Email</span>
                     </ListHeader>
-                    <ListBody textAlign="center">
-                        <span>ID</span>
-                        <span>Password</span>
-                        <span>Name</span>
-                        <span>Email</span>
-                    </ListBody>
+                    {users.map(user => {
+                        return (
+                            <ListBody textAlign="center">
+                                <span width="10%">{user.id}</span>
+                                <span width="50%">{user.password}</span>
+                                <span width="10%">{user.name}</span>
+                                <span width="30%">{user.email}</span>
+                            </ListBody>
+                        )
+                    })}
                 </div>
             </div>
         );
