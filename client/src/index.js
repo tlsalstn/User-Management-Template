cc229ba9b0e589d347f2e99035324f1336d81ea9
ShinMS <39144276+QMCHE@users.noreply.github.com>
2020-01-05 15:27:17 +0900

    [UPDATE] Store

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import { Provider } from 'mobx-react';
-import login from './stores/login';
+import { login, register, users } from './stores/userStore';
 
 const loginStore = new login();
+const registerStore = new register();
+const userStore = new users();
 
 ReactDOM.render(
-    <Provider login={loginStore}>
+    <Provider login={loginStore} register={registerStore} user={userStore}>
         <App />
     </Provider>,
     document.getElementById('root')
